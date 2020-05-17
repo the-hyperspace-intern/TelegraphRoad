1b82652fb084069827881dc5ab018dc1541ece7c
Ginkoe <ginkoe@protonmail.com>
2020-05-17 22:55:20 +0000

    Nothing special ! Just Parsing and more parsing

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,65 @@
 import { promises as fs } from 'fs';
 import bencode from 'bencode';
 import url, { parse as urlParser } from 'url';
-import dgram, { createSocket, Socket } from 'dgram';
+import { createSocket, Socket } from 'dgram';
 import crypto from 'crypto';
 
-function genSubcription(): Buffer {
-    const subPayload = Buffer.alloc(16);
-    // 0x417 0x27101980
-    subPayload.writeUInt32BE(0x417, 0);
-    subPayload.writeUInt32BE(0x27101980, 4);
-    subPayload.writeUInt32BE(0, 8);
-    const transactionId = crypto.randomBytes(4);
-    transactionId.copy(subPayload, 12);
-    return subPayload;
+namespace Utils {
+    export function leftShift(num: number, shift: number) {
+        return num * Math.pow(2, shift)
+    }
+
+    export function genId() {
+        const id = crypto.randomBytes(20);
+        Buffer.from('-GY001-').copy(id, 0);
+        return id;
+    }
+}
+
+namespace TorrentParser {
+    export function getFilesSize(metadata: any) {
+        let final_size = metadata.info.files.map((file: any) => file.length).reduce((a: number, b: number) => a + b);
+        return final_size;
+    }
+}
+
+namespace QueryGenerator {
+    export function genSubcription(): Buffer {
+        const subPayload = Buffer.alloc(16);
+        // 0x417 0x27101980
+        subPayload.writeUInt32BE(0x417, 0);
+        subPayload.writeUInt32BE(0x27101980, 4);
+        subPayload.writeUInt32BE(0, 8);
+        const transactionId = crypto.randomBytes(4);
+        transactionId.copy(subPayload, 12);
+        return subPayload;
+    }
+
+    export function genAnnouce(connectionID: Buffer): Buffer {
+        const announcePayload = Buffer.alloc(98);
+        connectionID.copy(announcePayload, 0); // ConnectionID
+        announcePayload.writeUInt32BE(1, 8); // Action
+        crypto.randomBytes(4).copy(announcePayload, 12); // Random TransactionID
+        // TODO : InfoHash
+        Utils.genId().copy(announcePayload, 36); // Peer ID
+        announcePayload.writeUInt8(0, 56); // Downloaded
+        // TODO : Number of Bytes left
+        return announcePayload
+    }
+}
+
+namespace ResponseParser {
+    export function subscription(response: Buffer) {
+        return {
+            action: response.readUInt32BE(0),
+            transactionId: response.readUInt32BE(4),
+            connectionId: response.slice(8),
+        }
+    }
 }
 
 class UDPHandler {
-    private socket: Socket;
+    socket: Socket;
     private url: url.UrlWithStringQuery;
     constructor(url: url.UrlWithStringQuery) {
         this.socket = createSocket('udp4');
@@ -45,24 +88,46 @@ class UDPHandler {
 async function main() {
     const data = await fs.readFile('src/Torrents/bunny.torrent');
     const decoded = bencode.decode(data);
+    console.log(decoded["announce-list"].map(e => e.map(c => c.toString())))
+    // console.log(JSON.stringify(decoded));
+    // console.log(TorrentParser.getFilesSize(decoded));
+
+    return;
+
     const announcer_url = urlParser(decoded.announce.toString());
     const udpHandler = new UDPHandler(announcer_url);
 
-    const subPayload = genSubcription();
-    const _r = await udpHandler.sendBuffer(subPayload).catch(_ => { throw "Timed Out" });
+    const subPayload = QueryGenerator.genSubcription();
+    const _subscription = await udpHandler.sendBuffer(subPayload).catch(_ => { throw "Timed Out" });
 
-    // Test if transaction handshake is successful
+
     const payloadTransaction = subPayload.slice(12, 16);
-    const response = {
-        action: _r.slice(0, 4),
-        transactionId: _r.slice(4, 8),
-        connectionId: _r.slice(8, 16)
-    };
-    console.log(response.transactionId)
-    console.log(payloadTransaction)
-    console.log(Buffer.compare(payloadTransaction, response.transactionId) == 0 ? "Same Transaction" : "oupsie ! Transaction Failed")
+    const subHandshake = ResponseParser.subscription(_subscription);
 
+    if (payloadTransaction.compare(subHandshake.connectionId) == 0) {    // Test if transaction handshake is successful
+        throw "Missed Transaction";
+    }
+
+    console.log("Transaction Successful, TransactionID:", payloadTransaction);
+
+    udpHandler.socket.close();
+    return udpHandler;
 
 }
 
-main().catch(e => console.log("An Error has occured", e));
+async function entry() {
+    await main().catch(e => {
+        console.log("An Error has occured", e, "Trying Again ~ !")
+        entry()
+    });
+}
+
+
+/* {
+   name: "ginkoe",
+   language: "javascript"
+   } */
+
+entry();
+
+
